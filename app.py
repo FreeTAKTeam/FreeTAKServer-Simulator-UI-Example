db4dc5e60c75f8f46818082e2c18d83bcc8a281f
LT <>
2021-01-29 12:16:27 +0000

    Add wander functionality

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os
 import random
 from flask import Flask, flash, session, request, redirect, render_template, send_file
 from werkzeug.utils import secure_filename
-from ftssim import gpx
+from ftssim import gpx, wander
 from threading import Thread
-
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -19,10 +18,20 @@ def allowed_file(filename):
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/gpx')
+def gpx_form():
+    return render_template('gpx.html')
+
+
+@app.route('/wander')
+def wander_form():
+    return render_template('wander.html')
+
+
+@app.route('/gpx', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -58,6 +67,27 @@ def upload_file():
             t1 = Thread(target=player.play_gpx)
             t1.start()
             return redirect('/')
+
+
+@app.route('/wander', methods=['POST'])
+def submit_wander():
+    if request.method == 'POST':
+        gap = request.form.get("gap")
+        speed = request.form.get("kph")
+        total_distance = request.form.get("total_distance")
+        distance_between_change = request.form.get("distance_between_change")
+        start_lat = request.form.get("start_lat")
+        start_lon = request.form.get("start_lon")
+        server = request.form.get("server")
+        callsign = request.form.get("callsign")
+        cot_type = request.form.get("cot_type")
+        wanderer = wander.Wander(server, total_distance_km=int(total_distance),
+                                 distance_between_change=int(distance_between_change), callsign=callsign,
+                                 speed_kph=int(speed), max_time_step_secs=int(gap), start_lat=float(start_lat),
+                                 start_lon=float(start_lon), cot_type=cot_type)
+        t1 = Thread(target=wanderer.start_wandering)
+        t1.start()
+        return redirect('/')
 
 
 @app.route('/failed')
